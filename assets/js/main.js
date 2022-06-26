5c94e258975e0698278e2afd47a191237840c303
Jaseem-v <muhammedjaseem753@gmail.com>
2022-06-26 23:11:40 +0530

    home page completed

---
+++ assets/js/main.js
@@ -0,0 +1,112 @@
+var swiper = new Swiper(".header_swiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
+
+
+///////////////////////////
+//
+var mySwiper = new Swiper('.gallery-swiper', {
+    // loop: true,
+    speed: 1000,
+    autoplay: {
+        delay: 3000,
+    },
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 80,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    initialSlide: 3,
+
+})
+
+
+// //////////////////////
+// clients 
+
+var swiper = new Swiper(".client-swiper", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    // pagination: {
+    //     el: ".swiper-pagination",
+    //     clickable: true,
+    // },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+        990: {
+            slidesPerView: 5,
+            spaceBetween: 60,
+        },
+        1400: {
+            slidesPerView: 6,
+            spaceBetween: 60,
+        }
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    loop: true,
+});
+
+
+
+///////////////////////////////////////
+// Navbar scroll
+
+$(window).on('scroll', function () {
+    if ($(window).scrollTop() > 200) {
+        $('#navbar').addClass('sticky')
+    } else {
+        $('#navbar').removeClass('sticky')
+
+    }
+
+});
+
+
+/////////////////////////////////////////////////////
+// Mobile menu
+const openBtn = document.querySelector(".open-btn")
+const nav_list = document.querySelector(".navigation__list")
+const overlay = document.querySelector(".overlay")
+const navContact = document.querySelector(".nav-contact")
+
+function mobileMenu() {
+    openBtn.classList.toggle("active");
+    nav_list.classList.toggle("active")
+    overlay.classList.toggle("hidden")
+    navContact.classList.toggle("hidden")
+}
+
+openBtn.addEventListener("click", mobileMenu)
+overlay.addEventListener("click", mobileMenu)
+
